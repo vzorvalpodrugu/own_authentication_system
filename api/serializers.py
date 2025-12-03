ce4cd74b2b7985014d34d6dda033dbea14e503bc
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-12-03 13:13:40 +0300

    class UserRegisterSerializer """Serializer for user registration"""

---
+++ api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from core.models import (
+    User,
+    Role,
+    BusinessElement,
+    AccessRule,
+    Product,
+    Order,
+    Store
+)
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    """Serializer for user registration"""
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    password_confirm = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'middle_name', 'password', 'password_confirm']
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError('Passwords do not match')
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
